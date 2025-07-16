08fa4af47f38d1747118ee135d6facb08b3f12bc
sundhar04 <sundhar170520@gmail.com>
2025-07-16 16:44:25 +0530

    commited the files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app= Flask(__name__)
+@app.route('/',methods=['GET'])
+def summ():
+    a=20
+    b=20
+    c=5
+    sum=a+b+c
+    return f"the total is hello {sum}"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
+#docker build -t yourusername/yourimage:latest .
+#docker push yourusername/yourimage:latest
